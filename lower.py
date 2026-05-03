120a5995af7bcab4847a9b923d8f47dcd8e60359
Dakota Smith <bazookaharwell@gmail.com>
2026-05-02 20:13:57 -0400

    Implement VU Lower Field type 3 decoding

--- lower.py
+++ lower.py
@@ -24,6 +24,46 @@ def _field_1(mnemonic: str, command: int, pc: int) -> str:
 
     return FORMAT.format(mnemonic_s, id_s, is_s, it_s)
 
+def _field_3(mnemonic: str, command: int, pc: int) -> str:
+    fs = FloatRegister.get_register((command >> 11) & 0x1F)
+    ft = FloatRegister.get_register((command >> 16) & 0x1F)
+    dest = FloatRegister.get_dest((command >> 21) & 0xF)
+    is_ = IntRegister.get_register((command >> 11) & 0x1F)
+    it_ = IntRegister.get_register((command >> 16) & 0x1F)
+
+    match mnemonic:
+        case "EATANxy" | "EATANxz" | "ELENG" | "ERLENG" | "ERSADD" | "ESADD" | "ESUM":
+            return f"{mnemonic:<{MNEMONIC_SIZE}} {f"{IntRegister.P},":<{REG_SIZE}} {fs}"
+        case "ILWR" | "ISWR":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{it_},":<{REG_SIZE}} ({is_}){dest}"
+        case "LQD":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{ft}{dest},":<{REG_SIZE}} (--{is_})"
+        case "LQI":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{ft}{dest},":<{REG_SIZE}} ({is_}++)"
+        case "MFIR":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{ft}{dest},":<{REG_SIZE}} {is_}"
+        case "MFP":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{ft}{dest},":<{REG_SIZE}} {IntRegister.P}"
+        case "MOVE":
+            if (ft == fs): return "NOP"
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{ft}{dest},":<{REG_SIZE}} {fs}{dest}"
+        case "MR32":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{ft}{dest},":<{REG_SIZE}} {fs}{dest}"
+        case "RGET" | "RNEXT":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{ft}{dest},":<{REG_SIZE}} {IntRegister.R}"
+        case "SQD":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{fs}{dest},":<{REG_SIZE}} (--{it_})"
+        case "SQI":
+            return f"{f"{mnemonic}.{dest}":<{MNEMONIC_SIZE}} {f"{fs}{dest},":<{REG_SIZE}} ({it_}++)"
+        case "WAITP" | "WAITQ":
+            return f"{mnemonic}"
+        case "XGKICK":
+            return f"{mnemonic:<{MNEMONIC_SIZE}} {is_}"
+        case "XITOP" | "XTOP":
+            return f"{mnemonic:<{MNEMONIC_SIZE}} {it_}"
+
+
+
 def _attach_top_bit(cmd: int, n: int):
     # Lower has a different field type depending on if the top bit is set,
     # so we'll just move it to LSB for opcodes so we can easily check
@@ -38,8 +78,35 @@ FIELD_1_TABLE = {
     0b110101_1: "IOR",
     0b110001_1: "ISUB",
 }
+FIELD_3_TABLE = {
+    0b11101_1111_00_1: "EATANxy",
+    0b11101_1111_01_1: "EATANxz",
+    0b11100_1111_10_1: "ELENG",
+    0b11100_1111_11_1: "ERLENG",
+    0b11100_1111_01_1: "ERSADD",
+    0b11100_1111_00_1: "ESADD",
+    0b11101_1111_10_1: "ESUM",
+    0b01111_1111_10_1: "ILWR",
+    0b01111_1111_11_1: "ISWR",
+    0b01101_1111_10_1: "LQD",
+    0b01101_1111_00_1: "LQI",
+    0b01111_1111_01_1: "MFIR",
+    0b11001_1111_00_1: "MFP",
+    0b01100_1111_00_1: "MOVE",
+    0b01100_1111_01_1: "MR32",
+    0b10000_1111_01_1: "RGET",
+    0b10000_1111_00_1: "RNEXT",
+    0b01101_1111_11_1: "SQD",
+    0b01101_1111_01_1: "SQI",
+    0b11110_1111_11_1: "WAITP",
+    0b01110_1111_11_1: "WAITQ",
+    0b11011_1111_00_1: "XGKICK",
+    0b11010_1111_01_1: "XITOP",
+    0b11010_1111_00_1: "XTOP",
+}
 FIELDS = [
-    (FIELD_1_TABLE, lambda cmd: _attach_top_bit(cmd, cmd & 0x3F), _field_1)
+    (FIELD_1_TABLE, lambda cmd: _attach_top_bit(cmd, cmd & 0x3F), _field_1),
+    (FIELD_3_TABLE, lambda cmd: _attach_top_bit(cmd, cmd & 0x7FF), _field_3)
 ]
 
 def decode(command: int, pc: int) -> str:
